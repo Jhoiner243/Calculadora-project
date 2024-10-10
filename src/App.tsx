fd8e85512ac89a7a9f76ad85b83f1493e40413ce
jhoiner243 <jhoinermartinez243@gmail.com>
2024-10-10 00:21:53 -0500

    Cambios

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function  App(): JSX.Element {
+
+  return (
+    <>
+    <header>Calculadora project</header > 
+    <div>
+      <main>
+        <p>Aqui se mostrarán los operadores</p>
+      </main>
+    </div>
+    </>
+  )
+}
+
+export default App
